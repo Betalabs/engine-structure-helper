48a65216c8153b2d494b09bc1be8b6b6b098a74f
marcelo <marcelo.gomes@betalabs.com.br>
2021-01-22 15:06:38 -0300

    tooltips and hints

---
+++ src/Structures/Component/Tooltip.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Betalabs\StructureHelper\Structures\Component;
+
+use Illuminate\Contracts\Support\Arrayable;
+
+class Tooltip implements Arrayable
+{
+    /** @var string */
+    private $message;
+
+    /**
+     * @var string
+     */
+    private $field;
+
+    /**
+     * @param string $message
+     */
+    public function __construct(
+        string $field,
+        string $message
+    ) {
+        $this->field = $field;
+        $this->message = $message;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * Return the index property.
+     *
+     * @return string
+     */
+    public function getField(): string
+    {
+        return $this->field;
+    }
+
+    /**
+     * @param string $message
+     * @return Tooltip
+     */
+    public function setMessage(string $message)
+    {
+        $this->message = $message;
+        return $this;
+    }
+
+    public function toArray(): array
+    {
+        return [$this->getField() => $this->getMessage()];
+    }
+}
